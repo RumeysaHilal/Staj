8d3bc911d62b29bdcd2782c6f2bd12328d62a570
Rümeysa Hilal <66912242+RumeysaHilal@users.noreply.github.com>
2021-09-04 10:24:41 +0300

    Delete 03.01 - Cuma directory

--- 03.01 - Cuma/dicttojfile.py
+++
@@ -1,9 +0,0 @@
-import json
-#Dictionary veri tipindeki verileri json dosyasına dönüştürür.
-kisi_dict = {
-    "isim" : "hilal",
-    "soyisim" : "sevinc"
-}
-
-with open("kisi.json","w") as json_dosya:
-    json.dump(kisi_dict,json_dosya)
--- 03.01 - Cuma/jfiletodict.py
+++
@@ -1,6 +0,0 @@
-import json
-#JSON dosyasındaki verileri alır.
-with open ("kisi.json") as f:
-    veri=json.load(f)
-
-print(veri["isim"])
