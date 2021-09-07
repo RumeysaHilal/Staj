07c5f4fd67c58af951436eb186d620606432a7eb
Rümeysa Hilal <66912242+RumeysaHilal@users.noreply.github.com>
2021-09-07 16:21:32 +0300

    Update linechart.py

--- 07.09 - Salı/linechart.py
+++ 07.09 - Salı/linechart.py
@@ -15,17 +15,17 @@ except:
 with open (arguman[1]) as f: # Liste şeklinde alındığı için ikinci dosyanın alınması için 1 indisini verdim.
      veri=json.load(f) #Json verilerinin bir sözlüğe aktarımını yaptım.
 
-x_ekseni=list() #Uzun bir veri setini alacağımız için veriye aktarım için tanımladım.
+x_ekseni=list() #Üzerinde çalışma yapacağım için liste değişkenleri oluşturdum.
 y_ekseni=list()
 
 for i in range(0,94):
-    x_ekseni.append(veri[i]["totallines"]) #Satırları bir listeye ekledim.
-    y_ekseni.append(veri[i]["totalchars"])
+    x_ekseni.append(veri[i]["totallines"]) #Satır sayılarını bir listeye ekledim.
+    y_ekseni.append(veri[i]["totalchars"]) #Karakter sayılarını bir listeye ekledim.
     
     
 plt.plot(x_ekseni,y_ekseni) #Listeye eklediğim verileri tablo için erlerine yazdım. 
 plt.title("Toplam Satır Sayısındaki Karakter Sayısı")
-plt.xlabel("Toplam Satir") 
-plt.ylabel("Toplam Karakter")
+plt.xlabel("Toplam Satir") #x ekseninin ismini verdim.
+plt.ylabel("Toplam Karakter") #y ekseninin ismini verdim.
 plt.grid(True) #Arka taraftaki çizgilerin belirgin olması için True değeri verdim.
 plt.show() #Çalıştırınca ekrana grafiğin gelmesi için show fonksiyonunu kullandım.
